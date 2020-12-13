5855a0d39c63f018c378bf1be40012d79c133ca2
leooosan <75056752+leooosan@users.noreply.github.com>
2020-12-12 22:50:20 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,31 +1,35 @@
 from pydantic import BaseModel
 
 
-class Orden(BaseModel):
+class Reserva(BaseModel):
     id: int
-    fecha: str
-    usuario: str
-    tienda: str
-    valor: float
+    quien_reserva: str
+    fecha_entrada: str
+    fecha_salida: str
+    numero_noches: int
+    tipo_habitacion: str
+    numero_personas: int
+    numero_habitacion: int
+    precio: int
 
 
-ordenes = {
-    1: Orden(id=1, fecha="20-11-2020", usuario="pepe", tienda="dominos", valor=2000),
-    2: Orden(id=2, fecha="20-11-2020", usuario="juan", tienda="pizzhut", valor=3000)
+reservas = {
+    1: Reserva(id=1, quien_reserva="Leonardo Santos",fecha_entrada="20-11-2020", fecha_salida="18-12-2020",numero_noches=15, tipo_habitacion="doble", numero_personas=2,numero_habitacion=106,precio=285000),
+    2: Reserva(id=2, quien_reserva="Andres Gomez",fecha_entrada="10-12-2020", fecha_salida="14-12-2020",numero_noches=4, tipo_habitacion="sencilla", numero_personas=1,numero_habitacion=109,precio=96000
 }
 
 
-def obtener_ordenes():
-    # Haga lo que tenga que hacer para conectarse a la base de datos y obtener todas las ordenes
-    lista_ordenes = []
-    for e in ordenes:
-        lista_ordenes.append(ordenes[e])
-    return lista_ordenes
+def obtener_reservas():
+    # Haga lo que tenga que hacer para conectarse a la base de datos y obtener todas las reservas
+    lista_reservas = []
+    for e in reservas:
+        lista_reservas.append(reservas[e])
+    return lista_reservas
 
 
-def crear_orden(orden: Orden):
-    if orden.id in ordenes:
+def crear_reserva(reserva: Reserva):
+    if reserva.id in reservas:
         return False
     else:
-        ordenes[orden.id] = orden
+        reservas[reserva.id] = reserva
         return True
